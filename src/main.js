2a59409f22db7f8d186830b63420c3972851ffc8
TripleNine666 <kolumb1709@gmail.com>
2023-02-05 10:42:29 +0300

    multiple routes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
 import NotFound from "./components/nav/NotFound.vue";
+import TeamFooter from "./components/teams/TeamFooter.vue";
+import UserFooter from "./components/users/UserFooter.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,7 +16,10 @@ const router = createRouter({
     {
       name: "teams",
       path: "/teams",
-      component: TeamsList,
+      components: {
+        default: TeamsList,
+        footer: TeamFooter,
+      },
       children: [
         {
           name: "team-members",
@@ -24,7 +29,14 @@ const router = createRouter({
         },
       ],
     },
-    { name: "users", path: "/users", component: UsersList },
+    {
+      name: "users",
+      path: "/users",
+      components: {
+        default: UsersList,
+        footer: UserFooter,
+      },
+    },
     { name: "not-found", path: "/:notFound(.*)", component: NotFound },
   ],
   linkActiveClass: "active",
